a082dd4b62e3d59d71e88b61de104162392b10ab
Jihoon Oh <jihoon.oh@edu.sait.ca>
2022-04-20 12:33:42 -0600

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Calculator extends JFrame {
+    public Calculator(){
+        setTitle("Calculator");
+        setVisible(true);
+        setSize(300,370);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+
+}
