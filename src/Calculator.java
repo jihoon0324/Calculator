f82eb8491973a79cff01246de4299fe9efd677ed
Jihoon Oh <jihoon.oh@edu.sait.ca>
2022-04-22 15:28:01 -0600

    add calculator in JTextField

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,15 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Calculator extends JFrame {
     private JTextField inputSpace;
     public Calculator(){
         setLayout(null);
         inputSpace = new JTextField();
+        inputSpace.setEditable(false);
+inputSpace.setBackground(Color.white);
+inputSpace.setHorizontalAlignment(JTextField.RIGHT);
+input
         setTitle("Calculator");
         setVisible(true);
         setSize(300,370);
